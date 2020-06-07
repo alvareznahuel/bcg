6157ba9f3db5e6a8de892a5ddcecdbd0638f045c
Nahuel Alvarez <31695006+alvareznahuel@users.noreply.github.com>
2020-06-07 16:27:14 -0300

    Revert "Feature/cards endpoint"

--- client/public/src/main.js
+++ client/public/src/main.js
@@ -1,10 +1,9 @@
 var app = new Vue({
   el: '#app',
   data: {
+    message: 'W.I.P.',
     user: null,
-    signedin: false,
-    stage: 0,
-    gamecode: "XDFTDO"
+    signedin: false
   },
   methods: {
     checkAuth: function checkAuth() {
@@ -14,46 +13,15 @@ var app = new Vue({
         this.user = sessionStorage.getItem("user");
         if(this.user) {
             this.signedin = true;
-            this.stage = 1;
         } else {
             this.signedin = false;
-            this.stage = 0;
         }
       } else {
         this.user = null;
         this.signedin = false;
-        this.stage = 0;
       }
       //console.log(this.signedin);
       return;
-    },
-    createGame: function createGame() {
-        this.stage = 2;
-        return;
-    },
-    joinGame: function joinGame() {
-        this.stage = 3;
-        return;
-    },
-    setupGame: function setupGame() {
-        alert("Defining game attributes...");
-        this.stage = 4;
-        return;
-    },
-    signinGame: function signinGame() {
-        alert("Checking game code...");
-        this.stage = 5;
-        return;
-    },
-    initGame: function initGame() {
-        alert("Activating the defined game...");
-        this.stage = 5;
-        return;
-    },
-    startGame: function startGame() {
-        alert("Launching the game board...");
-        this.stage = 6;
-        return;
     } 
   }
 })
